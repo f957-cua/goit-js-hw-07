01a33cdf317e40517026dcf4315842480334f23c
ivan_filipchyk <ivanfiliptschik00@gmail.com>
2021-02-28 19:37:35 +0200

    удалил лишнее

--- js/task-08.js
+++ js/task-08.js
@@ -30,7 +30,6 @@ function createBoxes(event) {
     const newDiv = document.createElement('div');
 
     for (let i = 0; i < amount; i++) {
-        console.log("Privet");
         const nextDiv = document.createElement('div');
 
         let width = 30 + 10*i;
@@ -62,59 +61,3 @@ refs.btnDestroyEl.addEventListener('click', removeBoxes)
 function removeBoxes() {
     refs.boxesEl.innerHTML = '';
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let amount = 0;
-
-// function onInputValue(event) {
-//     amount = Number(event.currentTarget.value);
-//     console.log(amount);
-//     return amount
-// }
-
-// refs.inputEl.addEventListener('input', onInputValue);
-// console.log(amount);
-
-// refs.btnRenderEl.addEventListener('click', () => createBoxes(amount))
-
-
-// function createBoxes() {
-//     const box = document.createElement('div');
-//     let width = 30;
-//     let height = 30;
-//     box.style.width = `${width}px`;
-//     box.style.height = `${height}px`;
-//     let x = Math.floor(Math.random() * 256);
-//     let y = Math.floor(Math.random() * 256);
-//     let z = Math.floor(Math.random() * 256);
-//     box.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
-//     if (amount === 1) {
-//         refs.boxesEl.append(box);
-//     };
-//     if (amount >= 1) {
-//         for (let i = 1; i <= amount; i += 1) {
-//             width += 10;
-//             height += 10;
-//             refs.boxesEl.append(box);
-//         }
-//     };
-// };
-
-
-
